83a930878fd8b24ac8238ceac7942b82f6b522c8
Erdem <erdemcanaz@gmail.com>
2024-07-28 13:38:44 +0300

    new students are added

--- Üyelik excelleri analizleri ve güncellemeleri/main.py
+++ Üyelik excelleri analizleri ve güncellemeleri/main.py
@@ -2,8 +2,12 @@
 import csv, time
 from objects import GLMember
 from objects import CardRequests
+from objects import Students
 
-PARAM_PRINT_ROWS = True
+PARAM_PRINT_GL_MEMBERS = False
+PARAM_PRINT_CARD_REQUESTS = False
+PARAM_PRINT_HASH_ASSIGNMENTS = False
+PARAM_PRINT_STUDENTS = False
 
 # fetch GL_members from a csv file ========================================
 GL_members = []
@@ -15,15 +19,16 @@ with open('Ground Lab Üyeler - Aktif üyeler.csv', 'r', encoding='utf-8') as fi
         ogrenci_adi = row[1]
         girebilite = row[2]
         ogrenci_no = row[3]
-        giris_yili = row[4]
-        tanitim = row[5]
-        elektronik = row[6]
-        atolye = row[7]
-        printer = row[8]
-        note = row[9]
-        GL_members.append(GLMember(row_no, ogrenci_adi, girebilite, ogrenci_no, giris_yili, tanitim, elektronik, atolye, printer, note))
+        bolum = row[4]
+        giris_yili = row[5]
+        tanitim = row[6]
+        elektronik = row[7]
+        atolye = row[8]
+        printer = row[9]
+        note = row[10]
+        GL_members.append(GLMember(row_no, ogrenci_adi, girebilite, ogrenci_no, bolum, giris_yili, tanitim, elektronik, atolye, printer, note))
 
-        if PARAM_PRINT_ROWS: print(f"{counter+1}: {ogrenci_adi} ({ogrenci_no})")
+        if PARAM_PRINT_GL_MEMBERS: GL_members[-1].print_object(counter)
 
 # Check if there is any duplicate student number in the GL_members
 student_numbers = []
@@ -49,10 +54,10 @@ with open('Masa kullanımı ile ilgili request (Yanıtlar) - Form Yanıtları 1.
 
         is_zero_hash = (hash == "$argon2id$v=19$m=2097152__t=2__p=4$2p4gW1kQc3+daOMV7G50NA$SBS9Uwsa+TJOskYOkx1lYrGbePpIEy/XVlz3ZfDvDGY")
         card_requests.append(CardRequests(zaman_damgasi, ad_soyad, ogrenci_no, hash, comments, is_zero_hash))
-        if PARAM_PRINT_ROWS: card_requests[-1].print_object(counter+1)
+
+        if PARAM_PRINT_CARD_REQUESTS: card_requests[-1].print_object(counter)
 
 # MATCH CARDS WITH PERSON ========================================
-
 for member in GL_members:
     for card in card_requests:
         if member.ogrenci_no == card.ogrenci_no:
@@ -63,13 +68,12 @@ for member in GL_members:
     member.set_active_hash()
 
 # PRINT THE RESULT ========================================
-for counter, member in enumerate(GL_members):
-    is_hash_assigned = member.activehash is not None
-    print(f"{counter}| {member.ogrenci_adi:<{30}} - ({member.ogrenci_no:<{15}}) -> Hash atandı: {is_hash_assigned:<{7}} | Hash sayısı: {len(member.student_hashes):<{5}}")
+if PARAM_PRINT_HASH_ASSIGNMENTS:
+    for counter, member in enumerate(GL_members):
+        is_hash_assigned = member.activehash is not None
+        print(f"{counter}| {member.ogrenci_adi:<{30}} - ({member.ogrenci_no:<{15}}) -> Hash atandı: {is_hash_assigned:<{7}} | Hash sayısı: {len(member.student_hashes):<{5}}")
 
-    
-
-Student = []
+students = []
 with open('Students - Sayfa1.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     next(reader)  # Ignore the first row
@@ -81,4 +85,40 @@ with open('Students - Sayfa1.csv', 'r', encoding='utf-8') as file:
         electronics_1 = row[4]
         electronics_2 = row[5]
         printer = row[6]
-        Student.append(GLMember(ad_soyad, ogrenci_no, hash, mechanics, electronics_1, electronics_2, printer))
+        students.append(Students(ad_soyad, ogrenci_no, hash, mechanics, electronics_1, electronics_2, printer))  
+
+        if PARAM_PRINT_STUDENTS: students[-1].print_object()
+
+# Update 'Students' list
+for GL_member in GL_members:
+    if GL_member.girebilite == False: continue
+    if GL_member.activehash is None: continue
+
+    for student in students: # If the student is in the GL_members list, update the student's hash
+        if GL_member.ogrenci_no == student.ogrenci_no:
+            student.hash = GL_member.activehash.hash
+
+            if student.mechanics != GL_member.return_mechanic_time():
+                print(f"Mechanic time is updated for {student.ad_soyad} from {student.mechanics} to {GL_member.return_mechanic_time()}")
+                student.mechanics = GL_member.return_mechanic_time()
+
+            if student.electronics_1 != GL_member.return_electronics_1_time():
+                print(f"Electronics 1 time is updated for {student.ad_soyad} from {student.electronics_1} to {GL_member.return_electronics_1_time()}")
+                student.electronics_1 = GL_member.return_electronics_1_time()
+
+            if student.electronics_2 != GL_member.return_electronics_2_time():
+                print(f"Electronics 2 time is updated for {student.ad_soyad} from {student.electronics_2} to {GL_member.return_electronics_2_time()}")
+                student.electronics_2 = GL_member.return_electronics_2_time()
+            
+            if student.printer != GL_member.return_printer_time():
+                print(f"Printer time is updated for {student.ad_soyad} from {student.printer} to {GL_member.return_printer_time()}")
+                student.printer = GL_member.return_printer_time()
+
+            break
+
+    else:
+        new_student = Students(GL_member.ogrenci_adi, GL_member.ogrenci_no, GL_member.activehash.hash, GL_member.return_mechanic_time(), GL_member.return_electronics_1_time(), GL_member.return_electronics_2_time(), GL_member.return_printer_time())
+        students.append(new_student)
+
+        print(f"New student is added to the 'Students' list: {new_student.ad_soyad} - ({new_student.ogrenci_no}, ($hash)), {new_student.mechanics}, {new_student.electronics_1}, {new_student.electronics_2}, {new_student.printer}")
+                
--- Üyelik excelleri analizleri ve güncellemeleri/objects.py
+++ Üyelik excelleri analizleri ve güncellemeleri/objects.py
@@ -1,10 +1,11 @@
 
 class GLMember:
-    def __init__(self, row_no:str = None, ogrenci_adi:str = None, girebilite:bool = None, ogrenci_no:str=None, giris_yili:str=None, tanitim:str=None, elektronik:str=None, atolye:str=None, printer:str=None, note:str=None):
+    def __init__(self, row_no:str = None, ogrenci_adi:str = None, girebilite:bool = None, ogrenci_no:str=None,bolum:str =None, giris_yili:str=None, tanitim:str=None, elektronik:str=None, atolye:str=None, printer:str=None, note:str=None):
         self.row_no = row_no
         self.ogrenci_adi = ogrenci_adi
-        self.girebilite = girebilite
+        self.girebilite = True if girebilite == "Girebilir" else False
         self.ogrenci_no = ogrenci_no
+        self.bolum = bolum
         self.giris_yili = giris_yili
         self.tanitim = tanitim
         self.elektronik = elektronik
@@ -16,6 +17,24 @@ class GLMember:
     
         self.student_hashes = []
 
+    def print_object(self, counter:int):
+        print()
+        print(f"{counter}:")
+        print(f"row_no: {self.row_no}")
+        print(f"ogrenci_adi: {self.ogrenci_adi}")
+        print(f"girebilite: {self.girebilite}")
+        print(f"ogrenci_no: {self.ogrenci_no}")
+        print(f"bolum: {self.bolum}")
+        print(f"giris_yili: {self.giris_yili}")
+        print(f"tanitim: {self.tanitim}")
+        print(f"elektronik: {self.elektronik}")
+        print(f"atolye: {self.atolye}")
+        print(f"printer: {self.printer}")
+        print(f"note: {self.note}")
+        print(f"activehash: {self.activehash}")
+        print(f"student_hashes: {self.student_hashes}")
+        print()
+
     def append_hash(self, hash:str):
         self.student_hashes.append(hash)
 
@@ -26,6 +45,18 @@ class GLMember:
                 self.activehash = hash
                 break
           
+    def return_mechanic_time(self, duration:str = "120"):
+        return "0" if len(self.atolye)==0 else duration
+    
+    def return_electronics_1_time(self, duration:str = "120"):
+        return "0" if len(self.elektronik)==0 else duration
+    
+    def return_electronics_2_time(self, duration:str = "120"):
+        return "0" if len(self.elektronik)==0 else duration
+    
+    def return_printer_time(self, duration:str = "180"):
+        return "0" if len(self.printer)==0 else duration
+    
 class CardRequests():
     def __init__(self, zaman_damgasi:str=None, ad_soyad:str =None, ogrenci_no:str = None, hash:str=None, comments:str = None, is_zero_hash:bool = None):
         self.zaman_damgasi = zaman_damgasi
@@ -46,8 +77,7 @@ class CardRequests():
         print(f"is_zero_hash: {self.is_zero_hash}")
         print()
 
-
-class Sudents():
+class Students():
     def __init__(self, ad_soyad:str=None, ogrenci_no:str =None, hash:str = None, mechanics:str=None, electronics_1:str = None, electronics_2:str = None, printer:str = None):
         self.ad_soyad = ad_soyad
         self.ogrenci_no = ogrenci_no
@@ -57,3 +87,13 @@ class Sudents():
         self.electronics_2 = electronics_2
         self.printer = printer
       
+    def print_object(self):
+        print()
+        print(f"ad_soyad: {self.ad_soyad}")
+        print(f"ogrenci_no: {self.ogrenci_no}")
+        print(f"hash: {self.hash}")
+        print(f"mechanics: {self.mechanics}")
+        print(f"electronics_1: {self.electronics_1}")
+        print(f"electronics_2: {self.electronics_2}")
+        print(f"printer: {self.printer}")
+        print()
