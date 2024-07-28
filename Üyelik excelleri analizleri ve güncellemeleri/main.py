1e4f1322071ecdce9f17d8e6a2a37ded8d851ab6
Erdem <erdemcanaz@gmail.com>
2024-07-28 11:07:36 +0300

    UTF8 encoding fix

--- Üyelik excelleri analizleri ve güncellemeleri/main.py
+++ Üyelik excelleri analizleri ve güncellemeleri/main.py
@@ -1,12 +1,10 @@
 # fetch users from a csv file
-
 import csv
 
 GL_members = []
 
-with open('Ground Lab Üyeler - Aktif üyeler.csv', 'r') as file:
+with open('Ground Lab Üyeler - Aktif üyeler.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     for row in reader:
         GL_members.append(row)
         print(row)
-
