e33618ec3bb20635bdffa751c62f4dd7a23561db
Erdem <erdemcanaz@gmail.com>
2024-07-28 11:19:46 +0300

    GL member fetch is finished

--- Üyelik excelleri analizleri ve güncellemeleri/main.py
+++ Üyelik excelleri analizleri ve güncellemeleri/main.py
@@ -1,12 +1,12 @@
-# fetch users from a csv file
+# fetch GL_members from a csv file
 import csv
-import 
+from objects import GLMember
 
 GL_members = []
-
 with open('Ground Lab Üyeler - Aktif üyeler.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
-    for row in reader:
+    next(reader)  # Ignore the first row
+    for counter, row in enumerate(reader):
         row_no = row[0]
         ogrenci_adi = row[1]
         girebilite = row[2]
@@ -17,4 +17,11 @@ with open('Ground Lab Üyeler - Aktif üyeler.csv', 'r', encoding='utf-8') as fi
         atolye = row[7]
         printer = row[8]
         note = row[9]
+        GL_members.append(GLMember(row_no, ogrenci_adi, girebilite, ogrenci_no, giris_yili, tanitim, elektronik, atolye, printer, note))
+
+        print(f"{counter+1}: {ogrenci_adi} ({ogrenci_no})")
+
+# fetch CARD informations from a csv file
+
+
 
