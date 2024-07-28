b7b0050e8ea05015b3f0d2ef262bf4875e65e204
Erdem <erdemcanaz@gmail.com>
2024-07-28 11:13:54 +0300

    .

--- Üyelik excelleri analizleri ve güncellemeleri/main.py
+++ Üyelik excelleri analizleri ve güncellemeleri/main.py
@@ -1,10 +1,20 @@
 # fetch users from a csv file
 import csv
+import 
 
 GL_members = []
 
 with open('Ground Lab Üyeler - Aktif üyeler.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     for row in reader:
-        GL_members.append(row)
-        print(row)
+        row_no = row[0]
+        ogrenci_adi = row[1]
+        girebilite = row[2]
+        ogrenci_no = row[3]
+        giris_yili = row[4]
+        tanitim = row[5]
+        elektronik = row[6]
+        atolye = row[7]
+        printer = row[8]
+        note = row[9]
+
--- Üyelik excelleri analizleri ve güncellemeleri/objects.py
+++ Üyelik excelleri analizleri ve güncellemeleri/objects.py
@@ -1,6 +1,7 @@
 
 class GLMember:
-    def __init__(self, ogrenci_adi:str = None, girebilite:bool = None, ogrenci_no:str=None, giris_yili:str=None, tanitim:str=None, elektronik:str=None, atolye:str=None, printer:str=None, note:str=None):
+    def __init__(self, row_no:str = None, ogrenci_adi:str = None, girebilite:bool = None, ogrenci_no:str=None, giris_yili:str=None, tanitim:str=None, elektronik:str=None, atolye:str=None, printer:str=None, note:str=None):
+        self.row_no = row_no
         self.ogrenci_adi = ogrenci_adi
         self.girebilite = girebilite
         self.ogrenci_no = ogrenci_no
